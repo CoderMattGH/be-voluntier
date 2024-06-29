44a9f249d8fea0b5b27bf73fe71b74bdd35981b7
GANDHIX <mattdixon2000@hotmail.com>
2024-06-29 23:21:26 +0100

    Add password key

--- src/db/data/types.ts
+++ src/db/data/types.ts
@@ -3,6 +3,7 @@ type VolUser = {
   vol_first_name: string,
   vol_last_name: string,
   vol_email: string,
+  vol_password: string,
   vol_contact_tel?: string,
   vol_avatar?: Blob,
   vol_bio?: string,
