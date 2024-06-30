43d66d6a0cf0c352b9f335c407168e1c25ab74b2
GANDHIX <mattdixon2000@hotmail.com>
2024-07-01 00:43:58 +0100

    Add initial logic to listings model

---
+++ src/models/listings.model.ts
@@ -0,0 +1,25 @@
+import { logger } from "../logger";
+import { db } from "../db";
+
+export function selectListings(visible = true) {
+  logger.debug(`In selectListings() in listings.model`);
+
+  let queryStr = 
+      `SELECT list_id, list_title, list_location, list_longitude, list_latitude, list_date,
+        list_time, list_duration, list_description, list_img, org_users.org_name, 
+        org_users.org_avatar 
+        FROM listings 
+        JOIN org_users on listings.list_org = org_users.org_id 
+        WHERE listings.list_visible = $1;`;
+
+  return db.query(queryStr, [visible])
+    .then(({rows}) => {
+      console.log(rows);
+
+      if (!rows) {
+        return Promise.reject({status: 404, msg: "No listings found!"});
+      }
+
+      return {articles: rows};
+    });
+};
