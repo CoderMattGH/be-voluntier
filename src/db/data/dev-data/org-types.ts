0934f5d1d079cb80abd22e1af857b059489a3840
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 23:10:13 +0100

    Added primary key

--- src/db/data/dev-data/org-types.ts
+++ src/db/data/dev-data/org-types.ts
@@ -2,36 +2,47 @@ import type {OrgType} from "../types";
 
 export const orgTypes: OrgType[] = [
   {
+    type_id: 1,
     type_title: "Health and Medical"
   },
   {
+    type_id: 2,
     type_title: "Humanitarian and Disaster Relief"
   },
   {
+    type_id: 3,
     type_title: "Environmental and Conservation"
   },
   {
+    type_id: 4,
     type_title: "Educational"
   },
   {
+    type_id: 5,
     type_title: "Animal Welfare"
   },
   {
+    type_id: 6,
     type_title: "Community Development"
   },
   {
+    type_id: 7,
     type_title: "Social Justice and Advocacy"
   },
   {
+    type_id: 8,
     type_title: "Arts and Culture"
   },
   {
+    type_id: 9,
     type_title: "Senior and Elderly"
   },
   {
+    type_id: 10,
     type_title: "Sports and Recreation"
   },
   {
+    type_id: 11,
     type_title: "Religious and Faith-Based"
   }
 ];
