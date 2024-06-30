a37c361885c28c4a2fc85fe8b818c1453a68ea85
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 02:30:43 +0100

    Add session table logic

--- src/db/seeds/seed.ts
+++ src/db/seeds/seed.ts
@@ -6,6 +6,28 @@ import { VolUser } from '../data/types';
 import authUtils from '../../auth/auth-utils';
 const { hashPassword } = authUtils;
 
+function setupSessionTable() : Promise<any> {
+  console.log("Setting up user session table!");
+
+  return db
+      .query(
+        `CREATE TABLE "session" (
+          "sid" varchar NOT NULL COLLATE "default",
+          "sess" json NOT NULL,
+          "expire" timestamp(6) NOT NULL
+        )
+        WITH (OIDS=FALSE);`)
+      .then(() => {
+        return db.query(
+            `ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid") 
+                NOT DEFERRABLE INITIALLY IMMEDIATE;`);
+      })
+      .then(() => {
+        return db.query(
+            `CREATE INDEX "IDX_session_expire" ON "session" ("expire");`);
+      });
+}
+
 function seed(volUsers: VolUser[]): Promise<any> {
   console.log("Seeding database!");
   
@@ -14,8 +36,9 @@ function seed(volUsers: VolUser[]): Promise<any> {
       .then(() => {
         console.log("Creating vol_user table!");
 
-        return db.query(`
-            CREATE TABLE vol_user (
+        return db
+          .query(
+            `CREATE TABLE vol_user (
               vol_id SERIAL PRIMARY KEY,
               vol_email VARCHAR(200) NOT NULL,
               vol_password VARCHAR(100) NOT NULL,
@@ -27,36 +50,41 @@ function seed(volUsers: VolUser[]): Promise<any> {
               vol_hours INT,
               vol_badges INT
             );`
-        );
+          );
       })
       .then(() => {
         console.log("Populating vol_user table!");
 
-        const insertVolUsersQueryStr = format(`
-            INSERT INTO vol_user 
-              (
-                vol_first_name,
-                vol_last_name,
-                vol_email,
-                vol_password,
-                vol_contact_tel,
-                vol_bio,
-                vol_hours
-              ) VALUES %L;`,
-            volUsers.map((volUser) => {
-              return [
-                volUser.vol_first_name,
-                volUser.vol_last_name,
-                volUser.vol_email,
-                hashPassword(volUser.vol_password),
-                volUser.vol_contact_tel,
-                volUser.vol_bio,
-                volUser.vol_hours
-              ];
-            })
+        const insertVolUsersQueryStr = format(
+          `INSERT INTO vol_user (
+            vol_first_name,
+            vol_last_name,
+            vol_email,
+            vol_password,
+            vol_contact_tel,
+            vol_bio,
+            vol_hours
+          ) VALUES %L;`,
+          volUsers.map((volUser) => {
+            return [
+              volUser.vol_first_name,
+              volUser.vol_last_name,
+              volUser.vol_email,
+              hashPassword(volUser.vol_password),
+              volUser.vol_contact_tel,
+              volUser.vol_bio,
+              volUser.vol_hours
+            ];
+          })
         );
 
         return db.query(insertVolUsersQueryStr);
+      })
+      .then(() => {
+        return setupSessionTable();
+      })
+      .then(() => {
+        console.log("Database successfully seeded!");
       });
 }
 
