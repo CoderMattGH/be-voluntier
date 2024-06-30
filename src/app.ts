6c089779dd3f1252e023e6193ed852cccbac6b5d
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 13:10:30 +0100

    Add error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import './env-parser';
 import {logger} from './logger';
 
-import express from 'express';
+import express, {NextFunction, Request, Response} from 'express';
 import cors from 'cors';
 import session from 'express-session';
 
 import {apiRouter} from './routes/api-router';
 import {sessionInit} from './auth/session-init';
 import * as loginController from './controllers/login.controller';
+
+import {CustomReqError} from './types';
 
 const corsConfig = {
   origin: true,
@@ -33,4 +35,27 @@ app.get('/', (req, res, next) => {
 app.post('/api/login', loginController.loginUser);
 app.get('/api/logout', loginController.logoutUser);
 
-// TODO: Error handling middleware
+// Handle psql errors
+app.use((err: NodeJS.ErrnoException, req: Request, res: Response, next: NextFunction) : void => {
+  logger.debug(`In psql error handler!`);
+
+  if(err.code === '22P02' || err.code === '23502' || err.code === '2201X'
+      || err.code === '2201W')
+    res.status(400).send({msg: 'Bad request!'})
+  else if(err.code === '23503')   // Violates Foreign Key constraint
+    res.status(404).send({msg: 'Resource not found!'})  
+  else if(err.code === '23505')   // Duplicate Primary Key found
+    res.status(409).send({msg: 'Resource already exists!'});
+  else
+    next(err);
+});
+
+// Handle misc errors
+app.use((err: CustomReqError, req: Request, res: Response, next: NextFunction) : void => {
+  logger.debug(`In misc. error handler!`);
+
+  if (err.status && err.msg)
+    res.status(err.status).send({msg: err.msg});
+  else
+    next(err);
+});
