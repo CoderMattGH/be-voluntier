800179043d08be5e30e22ac9ec484d0970ff4da5
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 11:54:22 +0100

    Rename connection.ts to index.ts

---
+++ src/db/index.ts
@@ -0,0 +1,17 @@
+import {ENV} from '../env-parser';
+
+import {Pool} from 'pg';
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error("PGDATABASE or DATABASE_URL not set!");
+}
+
+type Config = { connectionString?: string, max?: number };
+
+let config: Config = {};
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+export const db = new Pool(config);
