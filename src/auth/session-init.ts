645cccbfca46384dfc6e0f37703135b49fef2d2c
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 02:30:07 +0100

    Move session logic into own file

--- src/auth/session-init.ts
+++ src/auth/session-init.ts
@@ -1,6 +1,9 @@
 import { Express } from "express";
+import pgSession from 'connect-pg-simple';
+import db from "../db/connection";
 
 // Role: volunteer, organiser, admin.
+// TODO: Change role to Enum from String
 type UserSession = {
   id: number;
   email: string;
@@ -14,9 +17,12 @@ declare module "express-session" {
   }
 }  
 
-// TODO: Change secret and private
-function sessionInit(app: Express, session: Function) : void {
+// TODO: Change from session: any
+// TODO: Change and private secret
+function sessionInit(app: Express, session: any) {
+  const pgOptions = {pool: db, tableName: 'session'};
   const sessionConfig = {
+    store: new (pgSession(session))(pgOptions),
     secret: 'keyboard-cat',
     cookie: {secure: false},
     resave: true,
