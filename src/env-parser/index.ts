1568efb9bdd9b78b42898b766b47c856c4cd3c0b
GANDHIX <mattdixon2000@hotmail.com>
2024-06-30 11:53:44 +0100

    Add env parser

---
+++ src/env-parser/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+// Import this file to parse any env files
+export const ENV = process.env.NODE_ENV || 'development';
+
+dotenv.config({
+  path: `${__dirname}/../../.env.${ENV}`,
+});
+
+import {logger} from '../logger';
+logger.info("Parsed env file!");
