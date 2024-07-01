f15bae54ca00c9fe1fad1bc9bf1c62b7a4a5ba03
GANDHIX <mattdixon2000@hotmail.com>
2024-07-01 15:50:00 +0100

    Server options

--- src/listen.ts
+++ src/listen.ts
@@ -1,9 +1,10 @@
 import { logger } from "./logger";
 import { app } from "./app";
 
-const { PORT = 9090 } = process.env;
+// const { PORT = 9090 } = process.env;
+const PORT = 9090;
 const { IP_ADD = "127.0.0.1" } = process.env;
 
-app.listen(PORT, () => {
-  logger.info(`App listening on port ${IP_ADD}!`);
+app.listen(PORT, IP_ADD, () => {
+  logger.info(`App listening on port ${PORT} @ ${IP_ADD}!`);
 });
