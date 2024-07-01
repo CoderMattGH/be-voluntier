93620aa9ac79ce1f066e6546457fa4f82811bdd8
GANDHIX <mattdixon2000@hotmail.com>
2024-07-02 00:41:49 +0100

    formatting

--- src/db/data/dev-data/applications.ts
+++ src/db/data/dev-data/applications.ts
@@ -1,41 +1,41 @@
 import type { Application } from "../types";
 
-// TODO: Check skills are OK
+// TODO: Seeding Check skills are OK
 export const applications: Application[] = [
   {
     vol_id: 1,
     listing_id: 3,
     prov_confirm: false,
-    full_conf: false
+    full_conf: false,
   },
   {
     vol_id: 2,
     listing_id: 3,
     prov_confirm: false,
-    full_conf: false
+    full_conf: false,
   },
   {
     vol_id: 3,
     listing_id: 4,
     prov_confirm: true,
-    full_conf: false
-  },    
+    full_conf: false,
+  },
   {
     vol_id: 4,
     listing_id: 5,
     prov_confirm: true,
-    full_conf: true
-  },  
+    full_conf: true,
+  },
   {
     vol_id: 2,
     listing_id: 4,
     prov_confirm: false,
-    full_conf: true
-  },    
+    full_conf: true,
+  },
   {
     vol_id: 5,
     listing_id: 1,
     prov_confirm: false,
-    full_conf: false
-  },      
+    full_conf: false,
+  },
 ];
