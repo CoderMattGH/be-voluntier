6a676997a50fbc7233677e7eee03c8303d0e6f54
GANDHIX <mattdixon2000@hotmail.com>
2024-07-01 21:07:28 +0100

    Add selectOrgUserByEmail()

--- src/models/org-user.model.ts
+++ src/models/org-user.model.ts
@@ -1,6 +1,25 @@
 import { logger } from "../logger";
 import { db } from "../db";
 
+export function selectOrgUserByEmail(email: string) {
+  logger.debug(`In selectOrgUserByEmail() in org-user.model`);
+  logger.info(`Searching for org_user with email: ${email}`);
+
+  email = email.trim();
+
+  const queryStr = `SELECT * FROM org_users WHERE org_users.org_email ILIKE $1;`;
+
+  return db.query(queryStr, [email]).then((result) => {
+    const { rows } = result;
+
+    if (!rows.length) {
+      throw new Error("ORG_USER_NOT_FOUND");
+    }
+
+    return rows[0];
+  });
+}
+
 export function selectOrgUserById(user_id: string) {
   logger.debug("In selectOrgUserById() in org-user.model");
   logger.info(`Selecting user by Id: ${user_id}`);
