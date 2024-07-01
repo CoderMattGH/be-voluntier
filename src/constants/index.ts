303c2633cbdf1e39a7b98c3fd3cd2bf42f39ae3b
GANDHIX <mattdixon2000@hotmail.com>
2024-07-01 21:50:04 +0100

    Add constants file

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const ERR_MSG_PERMISSION_DENIED =
+  "You do not have permission to access this resource!";
+
+export const ERR_MSG_NOT_LOGGED_IN =
+  "You must be logged in to access this resource!";
