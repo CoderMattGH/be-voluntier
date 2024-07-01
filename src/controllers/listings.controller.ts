a0933fb4b52de58e15a74c87d028621232905b21
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-01 10:46:06 +0100

    add get listing endpoint with controller and model

--- src/controllers/listings.controller.ts
+++ src/controllers/listings.controller.ts
@@ -1,26 +1,42 @@
 import { logger } from "../logger";
 import { Request, Response, NextFunction } from "express";
-import * as listingsModel from '../models/listings.model';
+import * as listingsModel from "../models/listings.model";
 
 export function getListings(req: Request, res: Response, next: NextFunction) {
   logger.debug(`In getListings() in listings.controller`);
-  
+
   console.log(req.query);
 
   let visible = true;
-  if (req.query.visible && req.query.visible === "false")
-    visible = false;
-  
+  if (req.query.visible && req.query.visible === "false") visible = false;
 
-  listingsModel.selectListings(visible)
-      .then((articles) => {
-        res.status(200).send(articles);
-      })
-      .catch((err) => {
-        next(err);
-      });
+  listingsModel
+    .selectListings(visible)
+    .then((articles) => {
+      res.status(200).send(articles);
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
-export function getListing() {}
+export function getListing(req: Request, res: Response, next: NextFunction) {
+  const { listing_id } = req.params;
+  logger.debug(`In getListing() in listings.controller`);
+
+  console.log(req.query);
+
+  let visible = true;
+  if (req.query.visible && req.query.visible === "false") visible = false;
+
+  listingsModel
+    .selectListing(visible, listing_id)
+    .then((listing) => {
+      res.status(200).send(listing);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
 
 export function postListing() {}
