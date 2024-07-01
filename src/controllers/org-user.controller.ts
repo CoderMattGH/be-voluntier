4440c5648099842cf18d55c35eb5085b2b0f1bfe
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-01 13:11:16 +0100

    add controller and model to get org by id endpoint

--- src/controllers/org-user.controller.ts
+++ src/controllers/org-user.controller.ts
@@ -1,4 +1,22 @@
 import { logger } from "../logger";
+
+import * as orgUserModel from "../models/org-user.model";
 import { Request, Response, NextFunction } from "express";
 
-export function getOrgUserById() {}
+export function getOrgUserById(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { user_id } = req.params;
+  logger.debug(`In getOrgUserById() in org-user.controller`);
+
+  orgUserModel
+    .selectOrgUserById(user_id)
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
