4c395534216fc647f4cbcb3b9388a28812bd9286
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-02 16:13:56 +0100

    add testing for get skills

---
+++ src/__tests__/endpoints/api-get-skills.test.ts
@@ -0,0 +1,32 @@
+import request from "supertest";
+
+import { app } from "../../app";
+import { db } from "../../db";
+import { seed } from "../../db/seeds/seed";
+import { testData } from "../../db/data/test-data/";
+
+beforeEach(() => seed(testData));
+
+afterAll(() => db.end());
+
+type Skill = {
+  skill_id?: number;
+  skill_name: string;
+};
+
+describe("GET /api/skills", () => {
+  test("Returns all skills", () => {
+    return request(app)
+      .get("/api/skills")
+      .expect(200)
+      .then(({ body }) => {
+        const skills = body.skills;
+
+        expect(skills.length).toBe(16);
+
+        skills.forEach((skill: Skill) => {
+          expect(typeof skill).toBe("string");
+        });
+      });
+  });
+});
