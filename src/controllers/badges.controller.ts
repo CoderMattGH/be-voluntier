20cc59e08e8f6a7a4117f942f1378de9148753f4
GANDHIX <mattdixon2000@hotmail.com>
2024-07-02 02:02:47 +0100

    Added auth to api/badges/:vol_user_id

--- src/controllers/badges.controller.ts
+++ src/controllers/badges.controller.ts
@@ -1,15 +1,49 @@
 import { logger } from "../logger";
 import { Request, Response, NextFunction } from "express";
 import * as badgesModel from "../models/badges.model";
+import { checkUserCredentials } from "../auth/auth-utils";
 
 export function getBadges(req: Request, res: Response, next: NextFunction) {
-  const { user_id } = req.params;
   logger.debug(`In getBadges() in badges.controller`);
 
   badgesModel
-    .selectBadges(user_id)
+    .selectBadges()
     .then((badges) => {
-      res.status(200).send(badges);
+      res.status(200).send({ badges });
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
+export function getBadgesByUserId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  logger.debug(`In getBadgesByUserId() in badges.controller`);
+  const { user_id } = req.params;
+
+  if (Number.isNaN(user_id)) {
+    next({ status: 400, msg: "user_id is not a number!" });
+
+    return;
+  }
+
+  let userIdNum = Number(user_id);
+
+  // Check authorisation
+  const volAuthObj = checkUserCredentials(req, userIdNum, "volunteer");
+  if (!volAuthObj.authorised) {
+    next(volAuthObj.respObj);
+
+    return;
+  }
+
+  badgesModel
+    .selectBadgesByUserId(user_id)
+    .then((badges) => {
+      res.status(200).send({ badges });
     })
     .catch((err) => {
       next(err);
