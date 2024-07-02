390e435d8a3412bf5f7a662848d74b93407ff793
GANDHIX <mattdixon2000@hotmail.com>
2024-07-02 17:11:53 +0100

    Add favourite-orgs data

---
+++ src/db/data/dev-data/favourite-orgs.ts
@@ -0,0 +1,48 @@
+import type { FavouriteOrg } from "../types";
+
+export const favouriteOrgs: FavouriteOrg[] = [
+  {
+    vol_id: 1,
+    org_id: 2,
+  },
+  {
+    vol_id: 1,
+    org_id: 3,
+  },
+  {
+    vol_id: 1,
+    org_id: 2,
+  },
+  {
+    vol_id: 2,
+    org_id: 1,
+  },
+  {
+    vol_id: 4,
+    org_id: 1,
+  },
+  {
+    vol_id: 5,
+    org_id: 3,
+  },
+  {
+    vol_id: 6,
+    org_id: 6,
+  },
+  {
+    vol_id: 7,
+    org_id: 3,
+  },
+  {
+    vol_id: 7,
+    org_id: 4,
+  },
+  {
+    vol_id: 7,
+    org_id: 5,
+  },
+  {
+    vol_id: 7,
+    org_id: 6,
+  },
+];
