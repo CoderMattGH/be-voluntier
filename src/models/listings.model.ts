5c40cc4a0a7c2ca066714ab464b220e4e493ac55
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-02 21:43:42 +0100

    add testing to get listing

--- src/models/listings.model.ts
+++ src/models/listings.model.ts
@@ -53,7 +53,7 @@ export function selectListing(visible = true, listing_id: string) {
   logger.debug(`In selectListing() in listings.model`);
 
   let queryStr = `SELECT list_id, list_title, list_location, list_longitude, list_latitude, list_date,
-        list_time, list_duration, list_description, list_img, org_users.org_name, 
+        list_time, list_duration, list_description, list_img, list_org, org_users.org_name, 
         org_users.org_avatar 
         FROM listings 
         JOIN org_users on listings.list_org = org_users.org_id 
@@ -61,9 +61,7 @@ export function selectListing(visible = true, listing_id: string) {
         AND listings.list_id = $2;`;
 
   return db.query(queryStr, [visible, listing_id]).then(({ rows }) => {
-    console.log(rows);
-
-    if (!rows) {
+    if (rows.length === 0) {
       return Promise.reject({ status: 404, msg: "No listing found!" });
     }
 
