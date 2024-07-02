5cc1d8ca42400a201d3e4038c5fc6c86ed429898
Joseph-Lee98 <josephlee498@gmail.com>
2024-07-02 11:12:28 +0100

    add leaderboard endpoint

--- src/controllers/leaderboard.controller.ts
+++ src/controllers/leaderboard.controller.ts
@@ -1,4 +1,20 @@
 import { logger } from "../logger";
 import { Request, Response, NextFunction } from "express";
+import * as leaderboardModel from "../models/leaderboard.model";
 
-export function getLeaderboard() {}
+export function getLeaderboard(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  logger.debug(`In getLeaderboard() in leaderboard.controller`);
+
+  leaderboardModel
+    .selectLeaderboard()
+    .then((leaderboard) => {
+      res.status(200).send(leaderboard);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
