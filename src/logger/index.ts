233fc5cfff18586156892095038530024e9b0b58
GANDHIX <mattdixon2000@hotmail.com>
2024-07-03 00:12:40 +0100

    Formatting

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,20 +1,20 @@
-import '../env-parser';
+import "../env-parser";
 
-import winston from 'winston';
-const {combine, timestamp, printf, colorize, align} = winston.format;
+import winston from "winston";
+const { combine, timestamp, printf, colorize, align } = winston.format;
 
 // If LOGL env var not set, then default to 'error'
-const LOGL = process.env.LOGL || 'error';
+const LOGL = process.env.LOGL || "error";
 
 export const logger = winston.createLogger({
   level: LOGL,
   format: combine(
-      colorize({all: true}),
-      timestamp({format: 'YYYY-MM-DD hh:mm:ss.SSS A'}), 
-      align(),
-      printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
-    ),
-  transports: [new winston.transports.Console()]
+    colorize({ all: true }),
+    timestamp({ format: "YYYY-MM-DD hh:mm:ss.SSS A" }),
+    align(),
+    printf((info) => `[${info.timestamp}] ${info.level}: ${info.message}`)
+  ),
+  transports: [new winston.transports.Console()],
 });
 
 logger.info(`Initialised logger!`);
