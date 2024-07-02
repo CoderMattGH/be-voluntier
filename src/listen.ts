ca1aae47ae5ce7354630c687aec23de6250e6e14
GANDHIX <mattdixon2000@hotmail.com>
2024-07-02 02:10:47 +0100

    Add port number env-var

--- src/listen.ts
+++ src/listen.ts
@@ -1,10 +1,9 @@
 import { logger } from "./logger";
 import { app } from "./app";
 
-// const { PORT = 9090 } = process.env;
-const PORT = 9090;
+const { PORT = 9090 } = process.env;
 const { IP_ADD = "127.0.0.1" } = process.env;
 
-app.listen(PORT, IP_ADD, () => {
+app.listen(Number(PORT), IP_ADD, () => {
   logger.info(`App listening on port ${PORT} @ ${IP_ADD}!`);
 });
